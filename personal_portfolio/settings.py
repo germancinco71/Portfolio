1b43f1e0488e9bb57c2d18c0236286bfa21e087a
germancinco71 <germancinco17@gmail.com>
2024-12-12 22:54:32 +0800

    modify allowed host

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s00jsg-#+c0sdr6#827@byhywkc4lt(v8ej89ta9vp^dg7=khg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['germancinco.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
